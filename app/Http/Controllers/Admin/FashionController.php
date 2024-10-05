7ecf4cb9398c2b4b2e46518a43b373120c4d6824
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-10-05 11:18:14 +0000

    FashionController.phpに、index Actionを追加 #19

--- app/Http/Controllers/Admin/FashionController.php
+++ app/Http/Controllers/Admin/FashionController.php
@@ -44,4 +44,21 @@ class FashionController extends Controller
 
         return redirect('admin/fashion/create');
     }
+
+    // index Actionを追加
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Fashion::where('title', $cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Fashion::all();
+        }
+        return view('admin.fashion.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
 }
+
+
+
