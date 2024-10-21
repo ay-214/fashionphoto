d67d7ad8b6444041cfdb230400f8dc5a28a26319
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-10-21 10:54:09 +0000

    index.blade.phpを変更 #28

--- resources/views/admin/fashion/index.blade.php
+++ resources/views/admin/fashion/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.admin')
-@section('title', '投稿されたスタイリングフォトの一覧')
+@section('title', '投稿されたStyling Postの一覧')
 
 @section('content')
     <div class="container">
         <div class="row">
-            <h2>スタイリングフォト一覧</h2>
+            <h2>Styling Post List</h2>
         </div>
         <div class="row">
-            <div class="col-md-4">
-                <a href="{{ route('admin.fashion.add') }}" role="button" class="btn btn-primary">新規投稿</a>
+            <div class="col-md-2">
+                <a href="{{ route('admin.fashion.add') }}" role="button" class="btn btn-primary">新規ポスト</a>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-7">
                 <form action="{{ route('admin.fashion.index') }}" method="get">
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル：スタイリングポイント</label>
+                        <label class="col-md-6">スタイリングテーマ</label>
                         <div class="col-md-8">
                             <input type="text" class="form-control" name="cond_title" value="{{ $cond_title }}">
                         </div>
@@ -32,8 +32,8 @@
                         <thead>
                             <tr>
                                 <th width="10%">ID</th>
-                                <th width="20%">タイトル：スタイリングポイント</th>
-                                <th width="50%">本文：身長とか</th>
+                                <th width="20%">スタイリングテーマ</th>
+                                <th width="50%">スタイリングポイント</th>
                                 <th width="10%">操作</th>
                             </tr>
                         </thead>
